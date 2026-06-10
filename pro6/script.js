bd327bcf340f64b32c954ac664cce98c4b32ce2a
haneef.razah <haneef.rzah@gmail.com>
2026-06-10 18:08:52 +0500

    digital clock

---
+++ pro6/script.js
@@ -0,0 +1,17 @@
+const time=document.querySelector(".time");
+const timeformat=document.querySelector(".timeformat");
+
+document.addEventListener("DOMContentLoaded",()=>{
+setInterval(showTime,1000);
+});
+const showTime=()=>{
+let date=new Date();
+let hr=date.getHours();
+let mins=date.getMinutes();
+let sec=date.getSeconds();
+hr=hr<10?"0"+hr:hr;
+mins=mins<10?"0"+mins:mins;
+sec=sec<10?"0"+sec:sec;
+
+time.innerHTML=`${hr}:${mins}:${sec}`;   
+timeformat.innerHTML=hr>=12?"PM":"AM";   }
